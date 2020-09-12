2b4ed0beb09bb8b0f561d28059856b352b8e7ac2
Calvin Boey <xiiaotheif@hotmail.com>
2020-09-12 21:45:22 +0800

    Added Transform

---
+++ NightLight_Studio/Component/ComponentTransform.cpp
@@ -0,0 +1,12 @@
+#include "ComponentTransform.h"
+
+ComponentTransform::ComponentTransform() : _position{ 0.0f, 0.0f, 0.0f }, 
+	_rotation{ 0.0f, 0.0f, 0.0f }, _scale{ 1.0f, 1.0f, 1.0f }
+{
+
+}
+
+ComponentTransform::~ComponentTransform()
+{
+
+}
