f9383b0ace2eb8280c5acf90962eb272d54f79a0
Calvin Boey <xiiaotheif@hotmail.com>
2020-09-25 18:25:07 +0800

    ModelLoader stuffs
    ModelLoader stuffs

---
+++ GAM300_Graphics_System/Mesh.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "FBX_SDK/fbxsdk.h"
+
+namespace NS_GRAPHICS
+{
+	struct Mesh
+	{
+		FbxScene* fbxScene;
+
+		Mesh() = default;
+		~Mesh() = default;
+	};
+}
--- GAM300_Graphics_System/Source/ModelLoader.cpp
+++ GAM300_Graphics_System/Source/ModelLoader.cpp
@@ -4,11 +4,25 @@ namespace NS_GRAPHICS
 {
 	ModelLoader::ModelLoader()
 	{
-
+		_fbxManager = FbxManager::Create();
+		FbxIOSettings* settings = FbxIOSettings::Create(_fbxManager, IOSROOT);
+		_fbxManager->SetIOSettings(settings);
 	}
 
 	ModelLoader::~ModelLoader()
 	{
+		_fbxManager->Destroy();
+	}
+	void ModelLoader::LoadFBX(const std::string& fileName, Mesh& mesh)
+	{
+		FbxImporter* fbxImport = FbxImporter::Create(_fbxManager, "");
 
+		if (!fbxImport->Initialize(fileName.c_str(), -1, _fbxManager->GetIOSettings())) {
+			exit(-1);
+		}
+
+		mesh.fbxScene = FbxScene::Create(_fbxManager, fileName.c_str());
+		fbxImport->Import(mesh.fbxScene);
+		fbxImport->Destroy();
 	}
 }
