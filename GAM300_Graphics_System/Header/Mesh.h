30b23af589ce46e0d9d49aeeb62c5d78ca736037
Calvin Boey <xiiaotheif@hotmail.com>
2020-10-04 03:48:00 +0800

    Model Loader and Others
    Model Loader can get vertices and index for now.

--- GAM300_Graphics_System/Header/Mesh.h
+++ GAM300_Graphics_System/Header/Mesh.h
@@ -1,11 +1,20 @@
 #pragma once
-#include "FBX_SDK/fbxsdk.h"
+#include <vector>
+#include "glm/vec3.hpp"
+#include "glm/vec2.hpp"
 
 namespace NS_GRAPHICS
 {
 	struct Mesh
 	{
-		FbxScene* fbxScene;
+		std::vector<glm::vec3> _vertices;
+		std::vector<glm::vec2> _uv;
+		std::vector<glm::vec3> _normals;
+		std::vector<int> _indices;
+		std::vector<unsigned> _textures;
+		glm::vec3 _position;
+		glm::vec3 _scale;
+		glm::vec3 _rotation;
 
 		Mesh() = default;
 		~Mesh() = default;
