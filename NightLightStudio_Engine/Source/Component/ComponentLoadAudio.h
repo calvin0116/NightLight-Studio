4aa6bda3d4968df86f3841fb964e2780cee48b1f
YippyYaYa <yippyyeehaw@hotmail.com>
2020-10-30 17:56:21 +0800

    Fixed audio LocalVector bug

--- NightLightStudio_Engine/Source/Component/ComponentLoadAudio.h
+++ NightLightStudio_Engine/Source/Component/ComponentLoadAudio.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "LocalVector.h"  // replaces std::vector
 #include "..\..\ISerializable.h"
+#include <string>
 
 #include <filesystem>
 
@@ -9,8 +10,13 @@ namespace fs = std::filesystem;
 typedef class ComponentLoadAudio : public ISerializable
 {
 public:
+  struct data
+  {
+    char path[512];
+    char name[128];
+  };
   // path, user defined name
-  using data = std::pair <std::string, std::string>;
+  //using data = std::pair<const char*, const char*>;
   // Max sounds loaded.
   const static size_t MAX_SIZE = 24;
   // Always active
@@ -26,9 +32,12 @@ public:
     for (int i = 0; i < sound_array.Size(); ++i)
     {
       fs::path cur_path_name = sound_array[i].GetString();
+      data MyData;
       std::string MyName = cur_path_name.stem().string();
       std::string MyPath = cur_path_name.parent_path().string() + "/" + MyName + cur_path_name.extension().string();
-      _sounds.push_back(std::make_pair<std::string, std::string>(std::forward<std::string>(MyPath), std::forward<std::string>(MyName)));
+      strcpy_s(MyData.name, 128, MyName.c_str());
+      strcpy_s(MyData.path, 512, MyPath.c_str());
+      _sounds.push_back(MyData);
     }
   };
   virtual Value	Write() { return Value(); };
