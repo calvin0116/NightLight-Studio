45f8ab80f4b0190cf55ae73fc3c4ec4d9308023a
crystalweii <unified_lighterX@hotmail.com>
2020-11-01 23:58:21 +0800

    Minor failsafe checks

--- NightLightStudio_Engine/Source/Component/ComponentLoadAudio.h
+++ NightLightStudio_Engine/Source/Component/ComponentLoadAudio.h
@@ -28,17 +28,23 @@ public:
 
 	//read and write function for initialization from saved files
   void	Read(Value& val) override{ 
-    auto sound_array = val["ListOfSound"].GetArray();
-    for (int i = 0; i < sound_array.Size(); ++i)
-    {
-      fs::path cur_path_name = sound_array[i].GetString();
-      data MyData;
-      std::string MyName = cur_path_name.stem().string();
-      std::string MyPath = cur_path_name.parent_path().string() + "/" + MyName + cur_path_name.extension().string();
-      strcpy_s(MyData.name, 128, MyName.c_str());
-      strcpy_s(MyData.path, 512, MyPath.c_str());
-      _sounds.push_back(MyData);
-    }
+
+      if (val.FindMember("ListOfSound") == val.MemberEnd())
+          std::cout << "No Force data has been found" << std::endl;
+      else
+      {
+          auto sound_array = val["ListOfSound"].GetArray();
+          for (int i = 0; i < sound_array.Size(); ++i)
+          {
+              fs::path cur_path_name = sound_array[i].GetString();
+              data MyData;
+              std::string MyName = cur_path_name.stem().string();
+              std::string MyPath = cur_path_name.parent_path().string() + "/" + MyName + cur_path_name.extension().string();
+              strcpy_s(MyData.name, 128, MyName.c_str());
+              strcpy_s(MyData.path, 512, MyPath.c_str());
+              _sounds.push_back(MyData);
+          }
+      }
   };
   virtual Value	Write();
   virtual Value& Write(Value& val) { return val; };
