d5bb920967cde57c7ddabe30a9fa3656e5b11224
YippyYaYa <yippyyeehaw@hotmail.com>
2020-11-07 18:01:56 +0800

    Added test prop to DefaultTest.json

--- NightLightStudio_Engine/Source/Logic/CScripts/AllScripts.h
+++ NightLightStudio_Engine/Source/Logic/CScripts/AllScripts.h
@@ -4,6 +4,7 @@
 #include "TestScript.h"
 #include "../Player.h"
 #include "../CameraScript.h"
+#include "PossessScript.h"
 
 // Construct Scripts from string
 namespace AllScripts
@@ -11,10 +12,12 @@ namespace AllScripts
   IScript* Construct(std::string name)
   {
     if (name == "TestScript")
-        return dynamic_cast<IScript*>(new TestScript());
+      return dynamic_cast<IScript*>(new TestScript());
     else if (name == "Player")
-        return dynamic_cast<IScript*>(new Player());
+      return dynamic_cast<IScript*>(new Player());
     else if (name == "Camera")
-        return dynamic_cast<CameraScript*>(new CameraScript());
+      return dynamic_cast<CameraScript*>(new CameraScript());
+    else if (name == "Possess")
+      return dynamic_cast<PossessScript*>(new PossessScript());
   }
 }
