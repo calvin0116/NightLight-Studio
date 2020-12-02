d5f42f704185a90b665cdf7b97d03946a4c5e30c
crystalweiii <unified_lighterX@hotmail.com>
2020-12-03 01:38:15 +0800

    Ai controllable in script

--- NightLightStudio_Game/Asset/Scripts/ScriptEnemy.cs
+++ NightLightStudio_Game/Asset/Scripts/ScriptEnemy.cs
@@ -6,54 +6,42 @@ namespace Unicorn
 {
   public class ScriptEnemy : UniBehaviour
   {
-    public enum State
-    {
-      Human = 0,
-      Moth,
-      Possessed
-    }
     // Used player components
     RigidBody enemyRB;
-    Variables enemyVar;
-    //Variables enemyTrans;
-    //Navigator enemyNavigator; <- Get Navigator
+    //Variables enemyVar;
+    Navigator enemyNavigator; //<- Get Navigator
     // Other entity's components
 
 
-        // Player Stats, default values
-        public static float enemyForce = 100000.0f;  // Move force while human
+    // Player Stats, default values
+    public static float enemyForce = 100000.0f;  // Move force while human
     public static float maxEnemySpd = 500.0f;
-    //public static float maxMothSpd = 100.0f;
-    public static float moveForce = humnForce; // Current force
     // spawn point
     public static Vector3 spawnPoint;
     // Move dir for camera to player
-    public Vector3 fwd;
-    public Vector3 rht;
     Vector3 moveDir = new Vector3(0.0f, 0.0f, 0.0f);
 
-        //For init
-        public bool startPatroling = false;
-        public bool startsIdling = false;
-        public bool startsPaused = false;
-        //For update
+    //For init
+    public bool startPatroling = false;
+    public bool startIdling = false;
+    public bool startPaused = false;
+    //For update
     public bool isPatroling = true;
     public bool isIdling = false;
     public bool isPaused = false;
 
     public bool dectectsPlayer = false;     //Boolean to react to player later on
-        //Vector3 inVec;
-    // accumulated dt
+                                            //Vector3 inVec;
+                                            // accumulated dt
     private float accumulatedDt = 0.0f;
 
     public override void Init()
     {
-      //enemyRB = GetRigidBody(id);
-      //enemyVar = GetVariables(id);
-    //enemyNavigator = GetNavigator(id);
-    // Get default values from variables
-    //enemyForce = playerVar.GetFloat(0);
-    //maxEnemySpd = playerVar.GetFloat(1);
+      enemyRB = GetRigidBody(id);
+      enemyNavigator = GetNavigator(id);
+      // Get default values from variables
+      //enemyForce = playerVar.GetFloat(0);
+      //maxEnemySpd = playerVar.GetFloat(1);
       //moveForce = humnForce;
     }
 
@@ -63,8 +51,9 @@ namespace Unicorn
 
     public override void Update()
     {
+      Decision();
       Move();
-
+      Control();
     }
     public override void FixedUpdate()
     {
@@ -78,7 +67,7 @@ namespace Unicorn
       Transform otherTransform = GetTransform(other);
       if (otherTransform.Tag == 1)
       {
-        
+
       }
     }
 
@@ -90,63 +79,96 @@ namespace Unicorn
     {
     }
 
+    public void Decision()
+    {
+      //======Init phase====//
+      if (startIdling)
+      {
+        Console.WriteLine("Start Idle");
+        if (isPatroling)
+        {
+          enemyNavigator.isFollowing = false;
+          isPatroling = false;
+        }
+        startIdling = false;
+      }
+
+      if (startPaused)
+      {
+        Console.WriteLine("Start Pause");
+        if (isPatroling)
+        {
+          enemyNavigator.isPaused = true;
+          isPatroling = false;
+        }
+        startPaused = false;
+      }
+
+
+      if (startPatroling)
+      {
+        Console.WriteLine("Start Patrol");
+        if (enemyNavigator.isPaused)
+        {
+          //enemyNavigator->SetIsPaused(false);
+          enemyNavigator.isPaused = false;
+
+        }
+        else
+        {
+          //enemyNavigator->SetIsFollowing(true);
+          enemyNavigator.isFollowing = true;
+          isPatroling = false;
+        }
+        isPatroling = true;
+        startPatroling = false;
+      }
+    }
+
     // Enemy functions
     public void Move()
     {
-        //======Init phase====//
-        if (startsIdling)
-        {
-            if (isPatroling)
-            {
-                //enemyNavigator->SetIsFollowing(false);
-                isPatroling = false;
-            }
-            startsIdling = false;
-        }
 
-        if (startsPaused)
-        {
-            if (isPatroling)
-            {
-                enemyNavigator->SetIsPaused(true);
-                isPatroling = false;
-            }
-            startsPaused = false;
-        }
+      //Console.WriteLine("bla");
+
+      //======================//
+      //====Update phase=====//
+      if (isIdling || isPaused)   //Stop movement if idle/pause
+        return;
+      if (isPatroling)
+      {
+        //Any unique movement behaviour can be stated here
+
+      }
+      //=================//
 
 
-        if (startsPatroling)
-        {
-            // if (enemyNavigator->GetIsPaused())
-            {
-                // enemyNavigator->SetIsPaused(false);
+    }
+    public void Control()
+    {
 
-            }
-            // else
-            {
-                //enemyNavigator->SetIsFollowing(true);
-                isPatroling = false;
-            }
-            isPatroling = true;
-            startsPaused = startsPatroling;
-        }
-        //======================//
-        //====Update phase=====//
-        if (isIdling || isPaused)   //Stop movement if idle/pause
-            return;
+      //Key for debug
+      if (Input.GetKeyPress(VK.IKEY_M))
+      {
+        enemyNavigator.isPaused = !enemyNavigator.isPaused;
+        /*
         if (isPatroling)
-        {
-                //Any unique movement behaviour can be stated here
-        }
-            //=================//
-
-            //Key for debug
-            if (Input.GetKeyDown(VK.IKEY_R))
-            {
-                if (isPatroling)
-                    startsPaused = true;
-                else
-                    startsPatroling = true;
-            }
+          startPaused = true;
+        else
+          startPatroling = true;
+        */
+        Console.WriteLine("Is paused: %i" , enemyNavigator.isPaused);
+      }
+      //Speed up
+      if (Input.GetKeyPress(VK.IKEY_K))
+      {
+        enemyNavigator.speed += 1.0f;
+      }
+      //Speed up
+      if (Input.GetKeyPress(VK.IKEY_L))
+      {
+        enemyNavigator.speed += 1.0f;
+      }
+    }
   }
 }
