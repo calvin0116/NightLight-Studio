489efbc039c82356eb13ddff742af0c04d6e254f
wang yidi <cut3woodyx@hotmail.com>
2020-12-04 15:34:55 +0800

    fixed warnings, also removed alot of c++ script that are not in use, C++ script system deleted

--- NightLightStudio_Engine/Source/Component/ComponentCScript.h
+++ NightLightStudio_Engine/Source/Component/ComponentCScript.h
@@ -1,8 +1,8 @@
 #pragma once
 //#include "LocalVector.h"  // replaces std::vector
-#include "LocalString.h"
+//#include "LocalString.h"
 #include "..\..\ISerializable.h"
-#include "..\Logic\CScripts\IScript.h"
+//#include "..\Logic\CScripts\IScript.h"
 
 namespace ScriptType
 {
@@ -12,26 +12,26 @@ namespace ScriptType
 // Only BGM will use this the most.3D sound will follow this component's ObjID pos by default.
 typedef class ComponentCScript : public ISerializable
 {
-  static int _blkCtrl;
-public:
-  bool            _isActive;  // Expose, can edit
-  LocalString<20> _sName;     // Expose, can edit
-  IScript*        _pScript;   // Don't touch
-  int             _iTag;      // Expose, can edit. must be unique
-
-  ComponentCScript();
-	~ComponentCScript();
-  ComponentCScript(const ComponentCScript&);
-  ComponentCScript& operator=(const ComponentCScript&);
-
-	//read and write function for initialization from saved files
-  void	Read(Value&) override;
-  Value	Write() override;
-  Value& Write(Value& val) override { return val; }
-  virtual ComponentCScript* Clone()
-  {
-    ComponentCScript* newcomp = new ComponentCScript();
-    *newcomp = *this;
-    return newcomp;
-  }
+//  static int _blkCtrl;
+//public:
+//  bool            _isActive;  // Expose, can edit
+//  LocalString<20> _sName;     // Expose, can edit
+//  IScript*        _pScript;   // Don't touch
+//  int             _iTag;      // Expose, can edit. must be unique
+//
+//  ComponentCScript();
+//	~ComponentCScript();
+//  ComponentCScript(const ComponentCScript&);
+//  ComponentCScript& operator=(const ComponentCScript&);
+//
+//	//read and write function for initialization from saved files
+//  void	Read(Value&) override;
+//  Value	Write() override;
+//  Value& Write(Value& val) override { return val; }
+//  virtual ComponentCScript* Clone()
+//  {
+//    ComponentCScript* newcomp = new ComponentCScript();
+//    *newcomp = *this;
+//    return newcomp;
+//  }
 } CScriptComponent;
