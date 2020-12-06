30c73b23985989c1a588b5e66b25b9f99d8d7003
YippyYaYa <alexchew123@gmail.com>
2020-12-06 19:02:24 +0800

    Stop all chnls when initing.

--- NightLightStudio_Engine/Source/Audio/SystemAudio.cpp
+++ NightLightStudio_Engine/Source/Audio/SystemAudio.cpp
@@ -338,6 +338,9 @@ void SystemAudio::HandleTogglePlay(MessageTogglePlay& msg)
   if (_isPlaying && _Inited == false)
   {
     _Inited = true;
+    // Stop all chnls
+    for (int i = 0; i < s_MAX_CHANNELS; ++i)
+      _channels[i]->stop();
     MyGameInit();
   }
   else if (!_isPlaying)
