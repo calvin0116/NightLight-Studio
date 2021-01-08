21b5f368e1a5a80eb70ed87f6e40ea8120139d6e
Calvin Boey <xiiaotheif@hotmail.com>
2021-01-08 15:21:57 +0800

    Split animation from model
    Slowly seperating them

--- NightLightStudio_Engine/Source/Graphics/Model.h
+++ NightLightStudio_Engine/Source/Graphics/Model.h
@@ -1,11 +1,9 @@
 #pragma once
 #include <vector>
 #include <map>
-#include <unordered_map>
 #include "Mesh.h"
 #include "../Window/WndUtils.h"
-#include "../glm/mat4x4.hpp"
-#include "../glm/gtx/quaternion.hpp"
+#include "Animation.h"
 
 namespace NS_GRAPHICS
 {
@@ -23,27 +21,6 @@ namespace NS_GRAPHICS
 		glm::mat4 _boneTransform = glm::mat4(1.0f);
 
 		std::vector<BoneData> _childrenBones;
-	};
-
-	struct Animation
-	{
-		struct KeyFrames
-		{
-			std::vector<double> _posTime;
-			std::vector<double> _rotateTime;
-			std::vector<double> _scaleTime;
-
-			std::vector <glm::vec3> _position;
-			std::vector <glm::quat> _rotation;
-			std::vector <glm::vec3> _scale;
-
-			KeyFrames() = default;
-			~KeyFrames() = default;
-		};
-
-		std::string _animName;
-		double _time = 0.0f;
-		std::unordered_map<std::string, KeyFrames> _frames;
 	};
 
 	//This is just for storing all the ai nodes
