e3fb47240b737dd27328efe6e571385eb6c2ef10
YippyYaYa <yippyyeehaw@hotmail.com>
2021-02-08 13:51:32 +0800

    Camera Comp clean up for use. (In progress)

--- NightLightStudio_Engine/Source/Component/ComponentCamera.cpp
+++ NightLightStudio_Engine/Source/Component/ComponentCamera.cpp
@@ -2,18 +2,26 @@
 #include "Components.h"
 #include "ComponentManager.h"
 
-ComponentCamera::ComponentCamera()
-	: _isActive{ true }, _cameraID{ -1 }, _flag{ COLOUR },
-	_colour{ 0.5f,0.5f,0.5f }, _fov{ 45.0f }, _near{ 1.0f }, _far{ 50000.0f },
-	_x{}, _y{}, _w{}, _h{}, _depth{ 0 }
-{
-	strcpy_s(ser_name, "CameraComponent");
-}
+//ComponentCamera::ComponentCamera()
+//	: _isActive{ true }, _cameraID{ -1 }, _flag{ COLOUR },
+//	_colour{ 0.5f,0.5f,0.5f }, _fov{ 45.0f }, _near{ 1.0f }, _far{ 50000.0f },
+//	_x{}, _y{}, _w{}, _h{}, _depth{ 0 }
+//{
+//	strcpy_s(ser_name, "CameraComponent");
+//}
 
-ComponentCamera::ComponentCamera(const int& cameraID)
-	: _isActive{ true }, _cameraID{ cameraID }, _flag{ COLOUR },
-	_colour{ 0.5f,0.5f,0.5f }, _fov{ 45.0f }, _near{ 1.0f }, _far{ 50000.0f },
-	_x{}, _y{}, _w{}, _h{}, _depth{ 0 }
+//ComponentCamera::ComponentCamera(const int& cameraID)
+//	: _isActive{ true }, _cameraID{ cameraID }, _flag{ COLOUR },
+//	_colour{ 0.5f,0.5f,0.5f }, _fov{ 45.0f }, _near{ 1.0f }, _far{ 50000.0f },
+//	_x{}, _y{}, _w{}, _h{}, _depth{ 0 }
+//{
+//	strcpy_s(ser_name, "CameraComponent");
+//}
+
+ComponentCamera::ComponentCamera() : _isActive(true), _Offset(0.0f, 0.0f, 0.0f), _orientation(0.0f, 0.0f, 0.0f, 0.0f), _lerp(false), _followTgt(false), _tgt(""),
+_rotation_sensitivity(1000.0f), _drag_sensitivity(200.0f), _zoom_sensitivity(50.0f),
+_yaw(0.0f), _pitch(0.0f), _roll(0.0f),
+
 {
 	strcpy_s(ser_name, "CameraComponent");
 }
@@ -25,22 +33,22 @@ ComponentCamera::~ComponentCamera()
 
 //read and write function for initialization from saved files
 
-void ComponentCamera::SetCurrentCamera()
-{
-	if (_cameraID == -1)
-		_cameraID = NS_GRAPHICS::CameraSystem::GetInstance().CreateCamera(cameraPos, cameraTarget, pitch, yaw);
-
-	NS_GRAPHICS::CameraSystem::GetInstance().SetCurrentCamera(_cameraID);
-}
+//void ComponentCamera::SetCurrentCamera()
+//{
+//	if (_cameraID == -1)
+//		_cameraID = NS_GRAPHICS::CameraSystem::GetInstance().CreateCamera(cameraPos, cameraTarget, pitch, yaw);
+//
+//	NS_GRAPHICS::CameraSystem::GetInstance().SetCurrentCamera(_cameraID);
+//}
 
 inline void ComponentCamera::Read(Value& val)
 {
-	if (val.FindMember("isActive") == val.MemberEnd())
-		std::cout << "No active data has been found" << std::endl;
-	else
-		_isActive = val["isActive"].GetBool();
+	//if (val.FindMember("isActive") == val.MemberEnd())
+	//	std::cout << "No active data has been found" << std::endl;
+	//else
+	//	_isActive = val["isActive"].GetBool();
 
-	if (val.FindMember("FlagType") == val.MemberEnd())
+	/*if (val.FindMember("FlagType") == val.MemberEnd())
 		std::cout << "No active data has been found" << std::endl;
 	else
 	{
@@ -64,30 +72,30 @@ inline void ComponentCamera::Read(Value& val)
 			_colour.y = pos[1].GetFloat();
 			_colour.z = pos[2].GetFloat();
 		}
-	}
+	}*/
 
-	if (val.FindMember("FieldOfView") == val.MemberEnd())
-		std::cout << "No fov data has been found" << std::endl;
-	else
-	{
-		_fov = val["FieldOfView"].GetFloat();
-	}
+	//if (val.FindMember("FieldOfView") == val.MemberEnd())
+	//	std::cout << "No fov data has been found" << std::endl;
+	//else
+	//{
+	//	_fov = val["FieldOfView"].GetFloat();
+	//}
 
-	if (val.FindMember("NearPlane") == val.MemberEnd())
-		std::cout << "No near plane data has been found" << std::endl;
-	else
-	{
-		_near = val["NearPlane"].GetFloat();
-	}
+	//if (val.FindMember("NearPlane") == val.MemberEnd())
+	//	std::cout << "No near plane data has been found" << std::endl;
+	//else
+	//{
+	//	_near = val["NearPlane"].GetFloat();
+	//}
 
-	if (val.FindMember("FarPlane") == val.MemberEnd())
-		std::cout << "No far plane data has been found" << std::endl;
-	else
-	{
-		_far = val["FarPlane"].GetFloat();
-	}
+	//if (val.FindMember("FarPlane") == val.MemberEnd())
+	//	std::cout << "No far plane data has been found" << std::endl;
+	//else
+	//{
+	//	_far = val["FarPlane"].GetFloat();
+	//}
 
-	if (val.FindMember("XOffset") == val.MemberEnd())
+	/*if (val.FindMember("XOffset") == val.MemberEnd())
 		std::cout << "No x offset data has been found" << std::endl;
 	else
 	{
@@ -99,65 +107,65 @@ inline void ComponentCamera::Read(Value& val)
 	else
 	{
 		_y = val["YOffset"].GetFloat();
-	}
+	}*/
 
-	if (val.FindMember("Width") == val.MemberEnd())
-		std::cout << "No width data has been found" << std::endl;
-	else
-	{
-		_w = val["Width"].GetFloat();
-	}
+	//if (val.FindMember("Width") == val.MemberEnd())
+	//	std::cout << "No width data has been found" << std::endl;
+	//else
+	//{
+	//	_w = val["Width"].GetFloat();
+	//}
 
-	if (val.FindMember("Height") == val.MemberEnd())
-		std::cout << "No height data has been found" << std::endl;
-	else
-	{
-		_h = val["Height"].GetFloat();
-	}
+	//if (val.FindMember("Height") == val.MemberEnd())
+	//	std::cout << "No height data has been found" << std::endl;
+	//else
+	//{
+	//	_h = val["Height"].GetFloat();
+	//}
 
-	if (val.FindMember("Depth") == val.MemberEnd())
-		std::cout << "No depth data has been found" << std::endl;
-	else
-	{
-		_depth = val["Depth"].GetInt();
-	}
+	//if (val.FindMember("Depth") == val.MemberEnd())
+	//	std::cout << "No depth data has been found" << std::endl;
+	//else
+	//{
+	//	_depth = val["Depth"].GetInt();
+	//}
 }
 
 inline Value ComponentCamera::Write()
 {
-	Value val(rapidjson::kObjectType);
+	//Value val(rapidjson::kObjectType);
 
-	NS_SERIALISER::ChangeData(&val, "isActive", _isActive);		//Bool
+	//NS_SERIALISER::ChangeData(&val, "isActive", _isActive);		//Bool
 
-	switch (_flag)
-	{
-	case SKYBOX:
-		NS_SERIALISER::ChangeData(&val, "FlagType", rapidjson::StringRef("SKYBOX"));		//custom enum
-		break;
-	case COLOUR:
-		NS_SERIALISER::ChangeData(&val, "FlagType", rapidjson::StringRef("COLOUR"));		//custom enum
-		break;
-	default:
-		break;
-	}
+	//switch (_flag)
+	//{
+	//case SKYBOX:
+	//	NS_SERIALISER::ChangeData(&val, "FlagType", rapidjson::StringRef("SKYBOX"));		//custom enum
+	//	break;
+	//case COLOUR:
+	//	NS_SERIALISER::ChangeData(&val, "FlagType", rapidjson::StringRef("COLOUR"));		//custom enum
+	//	break;
+	//default:
+	//	break;
+	//}
 
-	Value colour(rapidjson::kArrayType);
-	colour.PushBack(_colour.x, global_alloc);
-	colour.PushBack(_colour.y, global_alloc);
-	colour.PushBack(_colour.z, global_alloc);
+	//Value colour(rapidjson::kArrayType);
+	//colour.PushBack(_colour.x, global_alloc);
+	//colour.PushBack(_colour.y, global_alloc);
+	//colour.PushBack(_colour.z, global_alloc);
 
-	NS_SERIALISER::ChangeData(&val, "BackgroundColour", colour);
+	//NS_SERIALISER::ChangeData(&val, "BackgroundColour", colour);
 
-	NS_SERIALISER::ChangeData(&val, "FieldOfView", _fov);
-	NS_SERIALISER::ChangeData(&val, "NearPlane", _near);
-	NS_SERIALISER::ChangeData(&val, "FarPlane", _far);
+	//NS_SERIALISER::ChangeData(&val, "FieldOfView", _fov);
+	//NS_SERIALISER::ChangeData(&val, "NearPlane", _near);
+	//NS_SERIALISER::ChangeData(&val, "FarPlane", _far);
 
-	NS_SERIALISER::ChangeData(&val, "XOffset", _x);
-	NS_SERIALISER::ChangeData(&val, "YOffset", _y);
-	NS_SERIALISER::ChangeData(&val, "Width", _w);
-	NS_SERIALISER::ChangeData(&val, "Height", _h);
+	//NS_SERIALISER::ChangeData(&val, "XOffset", _x);
+	//NS_SERIALISER::ChangeData(&val, "YOffset", _y);
+	//NS_SERIALISER::ChangeData(&val, "Width", _w);
+	//NS_SERIALISER::ChangeData(&val, "Height", _h);
 
-	NS_SERIALISER::ChangeData(&val, "Depth", _depth);
+	//NS_SERIALISER::ChangeData(&val, "Depth", _depth);
 
-	return val;
+	//return val;
 }
--- NightLightStudio_Engine/Source/Component/ComponentCamera.h
+++ NightLightStudio_Engine/Source/Component/ComponentCamera.h
@@ -2,6 +2,7 @@
 #include "..\\..\\ISerializable.h"
 #include "..\glm\glm.hpp"
 #include "../Graphics/CameraSystem.h"
+#include "..\Math\Vector.h"
 
 enum CAMERA_FLAG
 {
@@ -10,36 +11,37 @@ enum CAMERA_FLAG
 	ALL_FLAG
 };
 
-typedef class ComponentCamera : public ISerializable //: public IComponent
+// For gameplay only
+class ComponentCamera : public ISerializable //: public IComponent
 {
 public:
-	// Temporarily make them public for easy access
-
-	bool _isActive; // Temporarily set to true at beginning
-
-	// value is -1 if no camera is assigned
-	int _cameraID;
+	//int _cameraID; // value is -1 if no camera is assigned
 
 	// View variables
 	// Camera Vectors & Position(Point)
-	glm::vec3 cameraPos;
-	glm::quat m_orientation;
-	glm::vec3 cameraTarget; // Not currently in use for multi camera
+  bool _isActive; // Camera component is active.
+  NlMath::Vec3      _Offset; // X, Y, Dist offset to tgt.
+  //NlMath::Vec3      _color;
+	glm::quat         _orientation;
+  bool              _lerp;
+  bool              _followTgt;
+  LocalString<256>  _tgt;
+	//glm::vec3 cameraTarget; // Not currently in use for multi camera
 
 	// Dictates how fast the camera will move
-	float _rotation_sensitivity = 1000.f;
-	float _drag_sensitivity = 200.f;
-	float _zoom_sensitivity = 50.f;
+	float _rotation_sensitivity;
+	float _drag_sensitivity;
+	float _zoom_sensitivity;
 
 	// Variables for camera rotation
 	// Given in radians
-	float yaw;	// x-axis rotation (Rotation about y axis vector)
-	float pitch;  // y-axis rotation (Rotation about x axis vector)
-	float roll;   // z-axis rotation (Rotation about z axis vector)
+	float _yaw;	// x-axis rotation (Rotation about y axis vector)
+	float _pitch;  // y-axis rotation (Rotation about x axis vector)
+	float _roll;   // z-axis rotation (Rotation about z axis vector)
 
 	//Camera Variables
-	CAMERA_FLAG _flag;
-	glm::vec3 _colour;
+	//CAMERA_FLAG _flag;
+	//glm::vec3 _colour;
 	float _fov;
 
 	//Clipping Plane
@@ -47,19 +49,19 @@ public:
 	float _far;
 
 	//Viewport
-	float _x;
-	float _y;
-	float _w;
-	float _h;
+	//float _x;
+	//float _y;
+	//float _w;
+	//float _h;
 
 	//Draw order
-	int _depth;
+	//int _depth;
 
 	// Default constructor
 	ComponentCamera();
 
-	// Parametrized constructor 
-	ComponentCamera(const int& cameraID);
+	// Parametrized constructor
+	//ComponentCamera(const int& cameraID);
 
 	// Destructor, destroy all OpenGL objects via graphics system
 	~ComponentCamera();
@@ -70,7 +72,7 @@ public:
 	// Editor camera will still take precedence if editor is active
 	// utilizes _isActive
 	// Note that camera will be created here if no existing camera id is connected
-	void SetCurrentCamera();
+	//void SetCurrentCamera();
 
 	////////////////////////////////////////
 
