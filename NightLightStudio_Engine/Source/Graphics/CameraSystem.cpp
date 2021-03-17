6d2d25fee5560a37f32cd0b4f9b53c35f6b7926f
Calvin Boey <xiiaotheif@hotmail.com>
2021-03-17 18:51:00 +0800

    Update CameraSystem.cpp
    undo all changes

--- NightLightStudio_Engine/Source/Graphics/CameraSystem.cpp
+++ NightLightStudio_Engine/Source/Graphics/CameraSystem.cpp
@@ -346,7 +346,7 @@ namespace NS_GRAPHICS
 	{
 		// cursor setting
 		SYS_INPUT->GetSystemMousePos().SetToCenter(true);
-		//SYS_INPUT->GetSystemMousePos().SetCursorVisible(false);
+		SYS_INPUT->GetSystemMousePos().SetCursorVisible(false);
 
 		// set camera position
 		NS_GRAPHICS::Camera& cam = NS_GRAPHICS::CameraSystem::GetInstance().GetCamera();
@@ -494,7 +494,7 @@ namespace NS_GRAPHICS
 		{
 			// turn off cursor thingy
 			SYS_INPUT->GetSystemMousePos().SetToCenter(false);
-			//SYS_INPUT->GetSystemMousePos().SetCursorVisible(true);
+			SYS_INPUT->GetSystemMousePos().SetCursorVisible(true);
 		}
 		else
 		{
