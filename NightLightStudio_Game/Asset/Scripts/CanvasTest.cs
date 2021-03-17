55e1b45158907e955b3499496be0377446f3b6ab
YippyYaYa <alexchew123@gmail.com>
2021-03-17 22:04:40 +0800

    Canvas Test

--- NightLightStudio_Game/Asset/Scripts/CanvasTest.cs
+++ NightLightStudio_Game/Asset/Scripts/CanvasTest.cs
@@ -7,14 +7,17 @@ namespace Unicorn
   public class CanvasTest : UniBehaviour
   {
     public string UIName = "";
+    public string UIName2 = "";
     Canvas cnvs;
     UIElement ui1;
+    UIElement ui2;
 
     public override void Init()
     {
       cnvs = GetCanvas(id);
       Print(id.ToString());
       ui1 = cnvs.FindUI(UIName);
+      ui2 = cnvs.FindUI(UIName2);
     }
 
     public override void LateInit()
@@ -24,9 +27,9 @@ namespace Unicorn
     public override void Update()
     {
       if (ui1.OnClick())
-      {
         Print("Click Blue!");
-      }
+      if (ui2.OnClick())
+        Print("Click Red!");
       if (Input.GetKeyPress(VK.IKEY_SPACE))
       {
         cnvs.isActive = !cnvs.isActive;
